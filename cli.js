640231841c1959d572df19507121a4640f2dda5c
Giovanni <saturnowgep@hotmail.com>
2026-06-12 20:10:09 +0100

    Exercise 1 to 3

---
+++ cli.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const {Sequelize, Model, DataTypes, QueryTypes} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+})
+
+class Blog extends Model {};
+
+Blog.init({
+    id : {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.TEXT
+    },
+    url :{
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    title : {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    likes : {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    modelName: 'blogs'
+});
+
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT});
+        blogs.forEach(blog => {
+            console.log(blog.author+" : "+blog.title+", "+blog.likes+" likes");
+        })
+    } catch(error) {
+        console.log(error);
+    }
+}
+main();
