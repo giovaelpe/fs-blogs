6137c87b6354dd097293479139f9cc7a355cc4a5
Giovanni <saturnowgep@hotmail.com>
2026-06-16 20:04:20 +0100

    Exercise 17

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const blogRouter = require('./controllers/blogs.js')
 const {userRouter} = require('./controllers/users.js');
-const {DATABASE_URL, PORT} = require('./util/config.js');
+const {DATABASE_URL, PORT, TEST_DATABASE_URL} = require('./util/config.js');
+const User = require('./models/User.js');
+const Blog = require('./models/Blog.js');
 
 app.use(express.json());
 
@@ -10,6 +12,16 @@ app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+    res.status(200).end();
+});
+
+app.post("/deleteall", (req, res) => {
+    Blog.destroy();
+    User.destroy();
+    res.status(204).end();
+})
 
 app.listen(PORT, () => {
     console.log("Server running in port 3011");
