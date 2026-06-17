6273b59fcc78127abfb94ec8c179b09eaf40ade5
Giovanni <saturnowgep@hotmail.com>
2026-06-17 12:46:35 +0100

    fixing stuff

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 const Blog = require('./Blog');
 const User = require('./User');
+const {sequelize} = require('../util/db');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-Blog.sync({alter: true});
-User.sync({alter: true});
+//Blog.sync({alter: true});
+//User.sync({alter: true});
+sequelize.sync({alter: true});
 
 module.exports = {
     Blog,
